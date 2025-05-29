9ba6996dc19219114aca6969d1be551460f3a8c2
Burniee <52307044+Burniee@users.noreply.github.com>
2025-05-29 14:39:22 -0700

    Update script.js

--- src/server/script.js
+++ src/server/script.js
@@ -1,8 +1,8 @@
 import { gs } from '@servicenow/glide'
 
 export function showStateUpdate(current, previous) { 
-    const currentState = current.getValue('state2')
-    const previousState = previous.getValue('state2')
+    const currentState = current.getValue('state3')
+    const previousState = previous.getValue('state3')
 
-    gs.addInfoMessage(`state updated from "${previousState2}" to "${currentState2}"`)
+    gs.addInfoMessage(`state updated from "${previousState3}" to "${currentState3}"`)
 }

b80dab05f992f226f0cb77f374fb47b34cf79333
admin.wpt1 <test@example.com>
2025-05-29 14:59:12 -0700

    rsdfdsfds

--- src/server/script.js
+++ src/server/script.js
@@ -1,8 +1,8 @@
 import { gs } from '@servicenow/glide'
 
 export function showStateUpdate(current, previous) { 
-    const currentState = current.getValue('state4')
-    const previousState = previous.getValue('state4')
+    const currentState = current.getValue('state2')
+    const previousState = previous.getValue('state2')
 
-    gs.addInfoMessage(`state updated from "${previousState4}" to "${currentState4}"`)
+    gs.addInfoMessage(`state updated from "${previousState2}" to "${currentState2}"`)
 }
